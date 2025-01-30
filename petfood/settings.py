fc0135ffc47ab44aa1d6a32d9b56b160d46ef894
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2025-01-30 13:04:05 +0530

    new

--- petfood/settings.py
+++ petfood/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.171.176.145']
 
 
 # Application definition
