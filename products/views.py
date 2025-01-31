1f8d0ea4e06793fbfe76ef8023685c523e5dfc33
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2025-01-31 08:10:05 +0530

    page

--- products/views.py
+++ products/views.py
@@ -13,7 +13,7 @@ from rest_framework import status
 
 class ProductPagination(PageNumberPagination):
 
-    page_size = 5
+    page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
 
